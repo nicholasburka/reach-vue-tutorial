3d5a9df60c631469f308a428023863613024df69
nicholasburka <nburka@mac.com>
2021-04-30 15:16:43 -0400

    without Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Wallet v-on:connectWallet="connectWallet" v-on:fundAccount="fundAccount" :addr="this.addr" :bal="this.bal"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Wallet from './components/Wallet.vue'
+
+import * as reach from '@reach-sh/stdlib/ALGO.mjs'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Wallet
+  },
+  data: () => {
+    return {
+      acc: undefined,
+      addr: undefined,
+      balRaw: undefined,
+      bal: undefined
+      //currency: undefined
+    }
+  },
+  methods: {
+    async updateBalance() {
+      this.balRaw = await reach.balanceOf(this.acc)
+      this.bal = String(reach.formatCurrency(this.balRaw)).substr(0,6)
+    },
+    async connectWallet() {
+      console.log('conn')
+      console.log(this)
+      console.log(this.acc)
+      this.acc = await reach.getDefaultAccount()
+      this.addr = await this.acc.getAddress()
+      this.bal = await reach.balanceOf(this.acc)
+      console.log(this.acc)
+    },
+    async fundAccount() {
+      const fundAmt = 10
+      await reach.fundFromFaucet(this.acc, reach.parseCurrency(fundAmt))
+      console.log('funded')
+      this.updateBalance()
+    }
   }
 }
 </script>
---
+++ src/components/Wallet.vue
@@ -0,0 +1,71 @@
+<template>
+	<div id="wallet">
+		<img src="../assets/wallet.png" id="wallet-icon" v-on:click="connectWallet">
+		<div v-if="addr">
+			<p id="addr">{{addr}}</p>
+			<p id="bal">{{bal}} ALGO</p>
+			<div id="faucet">
+				<img src="../assets/faucet.png" id="faucet-icon" v-on:click="fundAccount">
+				<p>click here to fund your account</p>
+				<p class="subtext">(this may take several seconds, devnets only)</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ["addr", "bal"],
+		methods: {
+			connectWallet: function() {
+				console.log("conn")
+				this.$emit('connectWallet')
+			},
+			fundAccount: function() {
+				this.$emit('fundAccount')
+			}
+		}
+	}
+</script>
+
+<style scoped>
+#wallet {
+	position: absolute;
+	top: 1vh;
+	left: 1vw;
+	margin-top: .5vh;
+	margin-bottom: 0vh;
+	text-align: left;
+}
+p {
+	margin-top: .5vh;
+	margin-bottom: 0vh;
+}
+.subtext {
+	font-size: 1vw;
+}
+#wallet-icon {
+	max-height: 10vh;
+	width: auto;
+	display: block;
+}
+#faucet-icon {
+	max-height: 7vh;
+	left: 2vw;
+	width: auto;
+}
+#addr {
+	font-size: .5vw;
+}
+#bal {
+	font-size: 2vw;
+}
+#faucet {
+	color: white;
+	transition: .5s;
+}
+#faucet:hover {
+	color: black;
+	transition: .5s;
+}
+</style>
